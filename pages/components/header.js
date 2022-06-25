a96ecf3e07645c6647c67ca0903d48f54e1c6c3a
dongseob <51041187+dongseob@users.noreply.github.com>
2022-06-26 01:04:01 +0900

    redeploy

--- pages/components/header.js
+++ pages/components/header.js
@@ -51,11 +51,11 @@ export default function Header() {
             <Link href="/">
               <a href="#">
                 <span className="sr-only">Workflow</span>
-                <img
+                {/* <img
                   className="h-8 w-auto sm:h-10"
                   src="/vercel.svg"
                   alt=""
-                />
+                /> */}
               </a>
             </Link>
           </div>
@@ -185,11 +185,11 @@ export default function Header() {
             <div className="pt-5 pb-6 px-5">
               <div className="flex items-center justify-between">
                 <div>
-                  <img
+                  {/* <img
                     className="h-8 w-auto"
                     src="/vercel.svg"
                     alt="Workflow"
-                  />
+                  /> */}
                 </div>
                 <div className="-mr-2">
                   <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
